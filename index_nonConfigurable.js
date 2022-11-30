e03f48206b545b1618ad914029214ac99a34d937
QP-FeMu <91478987+QP-FeMu@users.noreply.github.com>
2022-11-30 15:29:12 +0100

    First Commit

---
+++ index_nonConfigurable.js
@@ -0,0 +1,109 @@
+// Load dependencies, e.g. Node.js modules
+const xsltProcessor = require('xslt-processor'); // to transform XML documents
+const fs = require("fs"); // to work with my file system
+const rp = require('request-promise').defaults({ jar: true }); // to enable then-catch controll structures
+const path = require("path"); // to provide utilities for working with file and directory paths
+const packageToRun = "Z2607_AZURE_UNIT_TEST"; // ABAP Unit-Testing Package in S4H
+
+
+
+/** Reads XML files needed to run AUnit Tests and transform to JUnit
+ * @return xml file with call to run abap unit test, xsl to transform from AUnit Result to JUnit Result
+ */
+function readXml() {
+    const xsltData = fs.readFileSync(path.resolve(__dirname, "./xml/aunit2junit.xsl"));  // read xslt-Data 
+    const xmlRunAbapUnitBuffer = fs.readFileSync(path.resolve(__dirname, "./xml/runAbapUnit.xml")); // read xml-Data
+    const xslt = xsltProcessor.xmlParse(xsltData.toString()); // apply xslt-Processor upon string of xslt-Data
+    const xmlRunAbapUnit = xmlRunAbapUnitBuffer.toString('utf8').replace("{{package}}", packageToRun); // insert the defined ABAP Test-Package in string of xml-Data
+    return { xmlRunAbapUnit, xslt};
+}
+
+
+
+/** HTTP-GET: Get CSRF-Token by calling GET with 'x-csrf-token: fetch'
+ * @returns Promise with the result of the call
+ */
+function getCSRFToken() {
+    const optionsGetCSRFToken = getCSRFTokenOptions();
+    return rp(optionsGetCSRFToken);
+}
+
+function getCSRFTokenOptions() {
+    return {
+        method: "GET",
+        rejectUnauthorized: false, // sonst Fehlermeldung - GGF. WIEDER ENTFERNEN !!!!
+        url: 'https://s4htcsaptui.westeurope.cloudapp.azure.com:44301/sap/opu/odata/SAP/adt_srv',
+        simple: false,
+        resolveWithFullResponse: true,
+        auth: {
+            user: "4112",
+            password: "mulitzka@SAP22",
+        },
+        headers: {
+            'X-CSRF-Token': 'fetch'
+        }
+    };
+}
+
+
+
+/** HTTP-POST: Run ABAP-Unit-Tests
+ * @param csrf token needed for the call
+ * @returns Promise with the result
+ */
+function runAbapUnitTest(xmlRunAbapUnit, xCSRFToken) {
+    const optionsRunUnitTest = getRunAbapUnitTestOptions(xmlRunAbapUnit, xCSRFToken);
+    return rp(optionsRunUnitTest);
+}
+
+function getRunAbapUnitTestOptions(xmlRunAbapUnit, xCSRFToken) {
+    return {
+        method: 'POST',
+        rejectUnauthorized: false, // sonst Fehlermeldung - GGF. WIEDER ENTFERNEN !!!!
+        url: 'https://s4htcsaptui.westeurope.cloudapp.azure.com:44301/sap/bc/adt/abapunit/testruns',
+        auth: {
+            user: "4112",
+            password: "mulitzka@SAP22",
+        },
+        headers: {
+            'x-csrf-token': xCSRFToken,
+            'Content-Type': "application/vnd.sap.adt.abapunit.testruns.config.v2+xml",
+            'Accept': 'application/vnd.sap.adt.abapunit.testruns.result.v1+xml'
+        },
+        body: xmlRunAbapUnit
+    };
+}
+
+
+
+/** Runs the ABAP-Unit-Test and converts them to JUnit format
+ * 1) Get CSRF-Token
+ * 2) Call Netweaver Server and get ABAP-Unit results
+ * 3) Transform result and save to output.xml
+ */
+function main() {
+    const { xmlRunAbapUnit, xslt } = readXml(); // read xml-Data and xslt-Data
+
+    csrfTokenPromise = getCSRFToken(); // GET: receive CSRF-Token
+
+    var runAbapUnitTestPromise = csrfTokenPromise.then(function (response) {
+        var csrfToken = response.headers['x-csrf-token']; // define CSRF-Token Variable
+        return runAbapUnitTest(xmlRunAbapUnit, csrfToken); // POST: run ABAP Unit-Tests
+    }
+    ).catch(function (err) {
+        console.error("ERROR_1: " + JSON.stringify(err));
+    });
+
+    runAbapUnitTestPromise.then(function (parsedBody) {
+        const xml = xsltProcessor.xmlParse(parsedBody); // define string of xslt-Data
+        const outXmlString = xsltProcessor.xsltProcess(xml, xslt); // define xml-String with ABAP Unit-Testing results
+        fs.writeFileSync("output.xml", outXmlString); // write xml-File with ABAP Unit-Testing results
+        console.log(outXmlString); // write ABAP Unit-Testing results into console
+    }
+    ).catch(function (err) {
+        console.error("ERROR_2: " + JSON.stringify(err));
+    });
+}
+
+console.log("-----------\n-----------\n-----------\nNEUER AUFRUF:\n...........")
+main();
---
+++ tests_general.js
@@ -0,0 +1,8 @@
+function main() {
+    console.log("1. Moin");
+}
+
+main();
+
+console.log("2. Moin");
+'1' === 1 ? console.log("Condition true") : console.log("Condition false");
